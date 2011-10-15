6359b9765eb4e4959e36d2fda4b7db19109b3e78
David Ball <davidrball@gmail.com>
2011-10-15 13:48:40 -0700

    sample quiz object stub

---
+++ quiz.js
@@ -0,0 +1,23 @@
+var Quiz = function(whateverInitiallizingParamsWeWant) {
+    
+};
+
+Quiz.prototype = {
+    onAnswer:function(blah) {
+        
+    },
+    onComlete:function(blah) {
+        
+    },
+    displayResults:function(blah) {
+        
+    }
+}
+
+
+/*usage
+
+var x = new Quiz();
+...
+x.displayResults();
+*/
